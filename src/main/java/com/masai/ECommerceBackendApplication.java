8c94d6785ab4c4c08d7a2c6f721d678bd249a3ce
Sameer Shaikh <sameershaikh01072004@gmail.com>
2025-07-09 13:21:48 +0530

    updating root

---
+++ src/main/java/com/masai/ECommerceBackendApplication.java
@@ -0,0 +1,43 @@
+package com.masai;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+
+@SpringBootApplication
+public class ECommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBackendApplication.class, args);
+		System.out.println("Ecommerce backend running !");
+	}
+	
+	@Bean
+public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+    return new OpenAPI()
+        .components(new Components().addSecuritySchemes("basicScheme",
+            new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+        .info(new Info()
+            .title("Shopx - E-Commerce Application REST API")
+            .version(appVersion)
+            .description(
+                    "Secure and scalable REST API built for a full-stack e-commerce platform.\n" +
+                    "Includes modules for Customer, Seller, Product, Cart, and Order management.\n" +
+                    "Session-based authentication system with 1-hour validity for both customers and sellers.\n" +
+                    "CRUD operations for all entities with real-time testing via Swagger UI.\n" +
+                    "Built using Spring Boot, Spring Data JPA, Hibernate, and PostgreSQL.\n"+
+					"\n \t-Created by Sameer Shaikh"
+            )
+            );
+}
+
+
+}
